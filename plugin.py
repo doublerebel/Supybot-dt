9095eedbf5c41a4ba932275df1f1ee55a25b9756
Charles Phillips <charles@doublerebel.com>
2012-07-16 17:41:45 -0700

    Also parse artist or label specifier

--- plugin.py
+++ plugin.py
@@ -4,7 +4,7 @@
 ###
 
 import os
-import string
+import re
 import urllib
 
 import supybot.utils as utils
@@ -22,6 +22,7 @@ class _Plugin(callbacks.Plugin):
 
     """Usage: @dt <searchterms> genre: <genrename>"""
     threaded = True
+
     def dt(self, irc, msg, args, things):
         """ <searchterms> [genre:<genrename>]
         
@@ -30,25 +31,28 @@ class _Plugin(callbacks.Plugin):
         opts = {}
         
         searchTerms = ' '.join(things)
-        searchTerms = string.split(searchTerms,'genre:')
-        if len(searchTerms) > 1:
-            genre = searchTerms[1]
-        else:
-            genre = False
-        searchTerms = searchTerms[0].strip()
+        reFacets = '((?:genre|artist|label):\s?[\-_a-zA-Z0-9&]+)'
         
         searchurl = 'http://api.digital-tunes.net/tracks'
-        if genre:
-            if genre == 'drum & bass' or genre == 'drum and bass' or genre == 'dnb':
-                genre = 'drum_and_bass'
-            searchurl += '/by_genre/' + genre
-        searchurl += '/search'
+        
+        match = re.search(reFacets, searchTerms)
+        if match:
+            match = match.group(0).split(':')
+            facet = match[0]
+            value = match[1].strip()
+            if facet == 'genre':
+                if value == 'drum&bass' or value == 'drum-and-bass' or value == 'd&b' or value == 'dnb':
+                    value = 'drum_and_bass'
+            searchurl += '/by_' + facet + '/' + value
+        else:
+            searchurl += '/search'
+            opts['term'] = searchTerms
+
         headers = utils.web.defaultHeaders
 
         # Construct a URL like:
-        # http://api.digital-tunes.net/tracks/by_genre/trance?key=fdee51fc1927dcc86093ga51efbdef63cbf5d&term=monkey&count=10
+        # http://api.digital-tunes.net/tracks/search?term=searchterms&key=fdee51fc1927dcc86093ga51efbdef63cbf5d&term=monkey&count=10
         
-        opts['term'] = searchTerms
         opts['key'] = self.registryValue('apiKey')
         numResults = self.registryValue('numResults')
         if numResults < 10:
